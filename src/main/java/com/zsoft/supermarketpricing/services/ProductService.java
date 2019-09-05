fc03f4394a6016345adfa6d812107142cc57ab4e
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-09-05 11:21:48 +0200

    5: TDD: adding productServiceTest

--- src/main/java/com/zsoft/supermarketpricing/services/ProductService.java
+++ src/main/java/com/zsoft/supermarketpricing/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.zsoft.supermarketpricing.services;
 
+import com.zsoft.supermarketpricing.models.Product;
 import com.zsoft.supermarketpricing.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ProductService {
     ProductRepository productRepository;
+    public Product getProductById(long productId) {
+        return null;
+    }
 }
---
+++ src/test/java/com/zsoft/supermarketpricing/services/ProductServiceTest.java
@@ -0,0 +1,37 @@
+package com.zsoft.supermarketpricing.services;
+
+import com.zsoft.supermarketpricing.models.Product;
+import com.zsoft.supermarketpricing.repository.ProductRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.junit.Assert.*;
+import java.util.Optional;
+import static org.mockito.Mockito.when;
+
+public class ProductServiceTest {
+    @Autowired
+    ProductService productService;
+    @MockBean
+    ProductRepository productRepository;
+
+    @Test
+    public void getProductByIdTest() {
+        //given
+        long idProduct = 1L;
+        Product product = Product.builder()
+                .id(idProduct)
+                .name("productNameExample")
+                .build();
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+
+        //when
+        Product maybeProduct = productService.getProductById(product.getId());
+
+        //then
+        assertNotNull(maybeProduct);
+        assertEquals(maybeProduct.getId(), product.getId());
+        assertEquals(maybeProduct.getName(), product.getName());
+
+    }
+}
